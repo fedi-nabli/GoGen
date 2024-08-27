b75700a8271f30434ae6cfd7c7df117fd9080769
fedi-nabli <fedinabli@gmail.com>
2024-08-27 08:41:38 +0100

    Added list of all supported languages

--- src/internal/languages/languages.go
+++ src/internal/languages/languages.go
@@ -1,16 +1,24 @@
 package languages
 
-const SupportedLanguages int = 6
+const SupportedLanguages int = 14
 const SupportedNodePackageManagers int = 3
 
 const (
-	C    = iota
-	RUST = iota
-	GO   = iota
-	MERN = iota
-	MEVN = iota
-	MEAN = iota
+	C                       = iota
+	C_MAKEFILE              = iota
+	C_CMAKE                 = iota
+	CXX_MAKEFILE            = iota
+	CXX_CMAKE               = iota
+	RUST                    = iota
+	RUST_LIB                = iota
+	GO                      = iota
+	FLASK                   = iota
+	NODE_EXPRESS            = iota
+	NODE_EXPRESS_TYPESCRIPT = iota
+	MERN                    = iota
+	MEVN                    = iota
+	MEAN                    = iota
 )
 
-var LanguagesArray = []string{"C", "Rust", "Go", "MERN", "MEVN", "MEAN"}
+var LanguagesArray = []string{"C", "C Makefile", "C CMake", "C++ Makefile", "C++ CMake", "Rust", "Rust Lib", "Go", "Flask", "Node Express", "Node Express Typescript", "MERN", "MEVN", "MEAN"}
 var NodePackageManagers = []string{"npm", "yarn", "pnpm"}
